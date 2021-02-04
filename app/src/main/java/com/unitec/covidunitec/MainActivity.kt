439822c2d1217a2a00d82ca5cf5c49aa3b1ca23b
LeonardoBanegas99 <leobanegas99@unitec.edu>
2021-02-04 12:54:18 -0600

    Prueba abrir 2do activity

--- app/src/main/java/com/unitec/covidunitec/MainActivity.kt
+++ app/src/main/java/com/unitec/covidunitec/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.unitec.covidunitec
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_IniciarSesion.setOnClickListener {
+            val intent = Intent(this,PerfilActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

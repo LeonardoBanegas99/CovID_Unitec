2695c5d24e1b4e16aea9dc6bcd667709ff1076ca
Carlos Nuila <cnuila14@icloud.com>
2021-03-10 02:09:21 -0600

    Infectados

--- app/src/main/java/com/unitec/covidunitec/PerfilActivity.kt
+++ app/src/main/java/com/unitec/covidunitec/PerfilActivity.kt
@@ -10,6 +10,7 @@ import android.os.Bundle
 import android.util.Log.d
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.main_frame.*
 import org.jetbrains.anko.toast
@@ -21,8 +22,11 @@ import kotlin.collections.ArrayList
 
 class PerfilActivity : AppCompatActivity() {
     var btn_escaneo: Button? = null
+    var btn_alertar: Button? = null
     var lista: ListView? = null
     var text_numCuenta: TextView? = null
+    var direccionBluetooth: String? = null
+    var alertar: Boolean = false
     var arrayList: ArrayList<String> = ArrayList()
     var arrayAdapter: ArrayAdapter<String>? = null
     var adapter: BluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
@@ -36,9 +40,25 @@ class PerfilActivity : AppCompatActivity() {
 
         text_numCuenta = findViewById(R.id.user)
         btn_escaneo = findViewById(R.id.btn_estado)
+        btn_alertar = findViewById(R.id.btn_alertar)
         lista = findViewById(R.id.list_signals)
         num_cuenta = intent.extras!!.get("num_cuenta") as String
         text_numCuenta!!.text = num_cuenta.toString()
+
+
+        db.collection("users").document(num_cuenta!!).get().addOnSuccessListener { doc ->
+            if (doc != null){
+                direccionBluetooth = doc.getString("BT_id")
+                alertar = doc.getBoolean("alertar")!!
+                if (alertar){
+                    Toast.makeText(this@PerfilActivity, "Estuviste cerca de alguien infectado", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this@PerfilActivity, "No existe este usuario", Toast.LENGTH_SHORT).show()
+            }
+        }.addOnFailureListener { exception ->
+            Toast.makeText(this@PerfilActivity, "Ocurrio un error", Toast.LENGTH_SHORT).show()
+        }
 
         btn_escaneo!!.setOnClickListener {
             //db.collection("usuarios").document("wv89Sx5nNmLs2CqIPPWU").get().addOnSuccessListener {
@@ -51,6 +71,29 @@ class PerfilActivity : AppCompatActivity() {
             var bool = adapter.startDiscovery()
             toast("Click " + bool)
         };
+
+        btn_alertar!!.setOnClickListener {
+            Toast.makeText(this@PerfilActivity, "", Toast.LENGTH_SHORT).show()
+            var seAdvirtio : Boolean = false
+            db.collection("interactions")
+                    .whereEqualTo("bt_id_contacto",direccionBluetooth)
+                    .get()
+                    .addOnSuccessListener { docs ->
+                        if (docs.size() != 0){
+                            for (doc in docs){
+                                var numCuenta : String? = doc.getString("num_cuenta_user")
+                                db.collection("users").document(numCuenta!!)
+                                        .update("alertar",true)
+                                        .addOnCanceledListener { Toast.makeText(this@PerfilActivity, "Ocurrio un error", Toast.LENGTH_SHORT).show() }
+                                db.collection("users").document(num_cuenta!!)
+                                        .update("status","Infectado")
+                            }
+                            Toast.makeText(this@PerfilActivity, "Las personas en contacto contigo han sido advertidas", Toast.LENGTH_SHORT).show()
+                        } else{
+                            Toast.makeText(this@PerfilActivity, "No hemos detectado contacto con nadie", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+        }
 
         var filter: IntentFilter = IntentFilter(BluetoothDevice.ACTION_FOUND)
         registerReceiver(receiver, filter)
